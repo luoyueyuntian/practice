409599a6c8004c7b7cd3a192d4c304fd09c3b65a
xuhuihui <anchex@163.com>
2018-01-21 16:56:32 +0800

    增加消息中心功能

--- heroes-tour/src/app/admin/message-center/message-center.component.ts
+++ heroes-tour/src/app/admin/message-center/message-center.component.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs/Observable';
 
 import Message from './../../model/message';
 import { MessageService } from './../../service/message.service';
+import Hero from '../../model/hero';
+import { HeroService } from './../../service/hero.service';
 
 @Component({
   selector: 'app-message-center',
@@ -10,14 +12,39 @@ import { MessageService } from './../../service/message.service';
   styleUrls: ['./message-center.component.css']
 })
 export class MessageCenterComponent implements OnInit {
-  messages: Message[];
+  messages: any[];
+  private heroes: Hero[];
   constructor(
-    private msgService: MessageService
+    private msgService: MessageService,
+    private heroService: HeroService
   ) {
+    this.heroService.getHeroes().subscribe((heroes: Hero[]) => {
+      this.heroes = heroes;
+    });
     this.msgService.getMessages()
       .subscribe((msgs: Message[]) => {
-        this.messages = msgs;
+        this.messages = [];
+        msgs.map((msg: Message) => {
+          this.messages.push({
+            id: msg.id,
+            auth: msg.auth,
+            message: msg.message,
+            addressee: this.parseAddressee(msg.addressee)
+          });
+        });
       });
+  }
+  private parseAddressee(id) {
+    if (id === 0) {
+      return 'All';
+    }
+    let addressee = '';
+    this.heroes.forEach((hero: Hero) => {
+      if (hero.id === id) {
+        addressee = hero.name;
+      }
+    });
+    return addressee;
   }
 
   ngOnInit() {
